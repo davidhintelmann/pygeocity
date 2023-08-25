12b519853c5af88c04874be07a73f9ee42a1188a
David Hintelmann <davidhin@mac.com>
2023-08-24 21:34:59 -0400

    upgrade to 0.2.0

--- README.md
+++ README.md
@@ -1,68 +1,162 @@
-# GeoNames - Find City Name with Population > 1000
+# GeoNames - Find City Name with a Population > 1000
 
-[Opendatasoft](https://public.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000/table/?disjunctive.cou_name_en&sort=name) has as a dataset from [GeoNames](https://www.geonames.org/about.html) which contains all cities with a population greater than a thousand people. This dataset will be used to create a reverse geocode function, called `search`. This project has been inspired by Richard Penman's [reverse-geocode](https://pypi.org/project/reverse-geocode/) project. Like Richard Penman's approach, I too am using a [k-d tree](https://en.wikipedia.org/wiki/K-d_tree) which is implemented using the [SciPy](https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.html) library.
+[Opendatasoft](https://public.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000/table/?disjunctive.cou_name_en&sort=name) has as a dataset from [GeoNames](https://www.geonames.org/about.html) which contains all cities with a population greater than a thousand people. This dataset will be used to create a reverse geocode function, called `search`. This project has been inspired by Richard Penman's [reverse-geocode](https://pypi.org/project/reverse-geocode/) project. Like Richard Penman's approach, I am also using a [k-d tree](https://en.wikipedia.org/wiki/K-d_tree) which is implemented using the [SciPy](https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.html) library.
 
-This package will take a latitude and longitude coordinate which will return the country, city name, alternate name(s), population, timezone, and exact coordinates of the city. 
+This package will take a latitude and longitude coordinate which will return the country, city name, alternate name(s), population, timezone, and exact coordinates for that city. 
 
 ## Dependencies
   * Python 3.11.4
+  * Numpy 1.25.0
   * Pandas 2.0.3
   * SciPy 1.11.1
-
-## Example usage:
-
-This package only has one function `search` and it has a single parameter `coordinate`.
-
-Enter latitude and longitude into the `search` function, as a list, to get your results.
-
-    >>> import geonames
-    >>> geonames.search([11.85812, -86.23922])
-
-    Outputs:
-        {
-            0: 3620170,
-            1: 'Diriamba',
-            2: 'Diriamba',
-            3: 'Diriamba,Diriambo',
-            4: 'NI',
-            5: 'Nicaragua',
-            6: 35008,
-            7: 'America/Managua',
-            8: '2018-08-08',
-            9: 'Nicaragua',
-            10: '11.85812, -86.23922',
-            11: ' -86.23922',
-            12: '11.85812'
-        }
-
-One can also enter a list of multiple latitudes and longitudes into the `search` function to get your results.
-
-    >>> geonames.search([[11.85812, -86.23922],[12.4825, -87.17304]])
-
-    Outputs:
-        {
-            'Geoname ID': {0: 3620170, 1: 3620269},
-            'Name': {0: 'Diriamba', 1: 'Corinto'},
-            'ASCII Name': {0: 'Diriamba', 1: 'Corinto'},
-            'Alternate Names': {0: 'Diriamba,Diriambo', 1: 'Corinto'},
-            'Country Code': {0: 'NI', 1: 'NI'},
-            'Country name EN': {0: 'Nicaragua', 1: 'Nicaragua'},
-            'Population': {0: 35008, 1: 19183},
-            'Timezone': {0: 'America/Managua', 1: 'America/Managua'},
-            'Modification date': {0: '2018-08-08', 1: '2018-08-08'},
-            'LABEL EN': {0: 'Nicaragua', 1: 'Nicaragua'},
-            'Coordinates': {0: '11.85812, -86.23922', 1: '12.4825, -87.17304'},
-            'Longitude': {0: ' -86.23922', 1: ' -87.17304'},
-            'Latitude': {0: '11.85812', 1: '12.4825'}
-        }
 
 ## Install
 
     pip install pygeocity
-    pip3 install pygeocity
 
-Make sure you install version 0.1.3
+Make sure you install version 0.2.0
 
 Use:
 
-    pip install pygeocity==0.1.3
+    pip install pygeocity==0.2.0
+
+## Example usage:
+
+This package only has one function `search` and it has a three parameters: 
+- `coordinate` Required - list or list(list)
+  - Search query allows a list of Latitude and Longitude, or a single pair which must be passed as list. It will return name of the nearest city in the same order which the coordinates were passed
+    
+    Example:
+    - single location: coordinates = [11.86, -86.24]
+    - multiple locations: coordinates = [[11.86, -86.24], [42, 49]]
+    
+- `k`: Optional - int > 1, Default: int = 1
+    - Number of of nearest neighbours to find for each set of coordinates. Must be a value greater than zero.
+    
+- `output`: Optional - 'df' or 'dict', default: output = 'df'
+    - Which format to return to users. Either pandas DataFrame or dictionary.
+    
+- `ascending`: Optional - True or False, default: None
+  - If the format should be sorted by ascending, descending population, or no sorting at all. This can help in finding the nearest city with the largest population (might need to increase k).
+
+### Code Examples
+
+- Enter latitude and longitude into the `search` function, as a list, to get your results.
+
+```python
+>>> import geonames
+>>> geonames.search([11.85812, -86.23922], output='dict')
+
+Outputs:
+{'Geoname ID': {0: 3620170},
+ 'Name': {0: 'Diriamba'},
+ 'ASCII Name': {0: 'Diriamba'},
+ 'Alternate Names': {0: 'Diriamba,Diriambo'},
+ 'Country Code': {0: 'NI'},
+ 'Country name EN': {0: 'Nicaragua'},
+ 'Population': {0: 35008},
+ 'Timezone': {0: 'America/Managua'},
+ 'Modification date': {0: '2018-08-08'},
+ 'LABEL EN': {0: 'Nicaragua'},
+ 'Coordinates': {0: '11.85812, -86.23922'},
+ 'Longitude': {0: '-86.23922'},
+ 'Latitude': {0: '11.85812'}}
+```
+
+
+
+- One can also enter a list of multiple latitudes and longitudes into the `search` function to get your results.
+
+```python
+>>> geonames.search([[11.85812, -86.23922],[12.4825, -87.17304]], output='dict')
+
+Outputs:
+{'CoordIndex': {0: 1, 1: 0},
+ 'Geoname ID': {0: 3620269, 1: 3620170},
+ 'Name': {0: 'Corinto', 1: 'Diriamba'},
+ 'ASCII Name': {0: 'Corinto', 1: 'Diriamba'},
+ 'Alternate Names': {0: 'Corinto', 1: 'Diriamba,Diriambo'},
+ 'Country Code': {0: 'NI', 1: 'NI'},
+ 'Country name EN': {0: 'Nicaragua', 1: 'Nicaragua'},
+ 'Population': {0: 19183, 1: 35008},
+ 'Timezone': {0: 'America/Managua', 1: 'America/Managua'},
+ 'Modification date': {0: '2018-08-08', 1: '2018-08-08'},
+ 'LABEL EN': {0: 'Nicaragua', 1: 'Nicaragua'},
+ 'Coordinates': {0: '12.4825, -87.17304', 1: '11.85812, -86.23922'},
+ 'Longitude': {0: '-87.17304', 1: '-86.23922'},
+ 'Latitude': {0: '12.4825', 1: '11.85812'}}
+```
+
+- You can also search for more than one nearest neighbour for each Latitude & Longitude pair entered. Here we search for two nearest neighbours.
+
+```python
+>>> geonames.search([[11.85812, -86.23922],[12.4825, -87.17304]], output='dict', k=2)
+
+Outputs:
+{'CoordIndex': {0: 0, 1: 0, 2: 1, 3: 1},
+ 'Geoname ID': {0: 3620163, 1: 3620170, 2: 3619451, 3: 3620269},
+ 'Name': {0: 'Dolores', 1: 'Diriamba', 2: 'El Realejo', 3: 'Corinto'},
+ 'ASCII Name': {0: 'Dolores', 1: 'Diriamba', 2: 'El Realejo', 3: 'Corinto'},
+ 'Alternate Names': {0: 'Dolores',
+  1: 'Diriamba,Diriambo',
+  2: 'El Realejo,Realejo',
+  3: 'Corinto'},
+ 'Country Code': {0: 'NI', 1: 'NI', 2: 'NI', 3: 'NI'},
+ 'Country name EN': {0: 'Nicaragua',
+  1: 'Nicaragua',
+  2: 'Nicaragua',
+  3: 'Nicaragua'},
+ 'Population': {0: 7065, 1: 35008, 2: 6208, 3: 19183},
+ 'Timezone': {0: 'America/Managua',
+  1: 'America/Managua',
+  2: 'America/Managua',
+  3: 'America/Managua'},
+ 'Modification date': {0: '2018-08-08',
+  1: '2018-08-08',
+  2: '2018-08-08',
+  3: '2018-08-08'},
+ 'LABEL EN': {0: 'Nicaragua', 1: 'Nicaragua', 2: 'Nicaragua', 3: 'Nicaragua'},
+ 'Coordinates': {0: '11.85672, -86.21552',
+  1: '11.85812, -86.23922',
+  2: '12.54333, -87.16517',
+  3: '12.4825, -87.17304'},
+ 'Longitude': {0: '-86.21552', 1: '-86.23922', 2: '-87.16517', 3: '-87.17304'},
+ 'Latitude': {0: '11.85672', 1: '11.85812', 2: '12.54333', 3: '12.4825'}}
+```
+
+- You can also search for more than one nearest neighbour for each Latitude & Longitude pair entered. Here we search for two nearest neighbours, and sort by the cities population in descending order.
+
+```python
+>>> geonames.search([[11.85812, -86.23922],[12.4825, -87.17304]], output='dict', k=2, ascending=False)
+
+Outputs:
+{'CoordIndex': {0: 0, 1: 0, 2: 1, 3: 1},
+ 'Geoname ID': {0: 3620170, 1: 3620163, 2: 3620269, 3: 3619451},
+ 'Name': {0: 'Diriamba', 1: 'Dolores', 2: 'Corinto', 3: 'El Realejo'},
+ 'ASCII Name': {0: 'Diriamba', 1: 'Dolores', 2: 'Corinto', 3: 'El Realejo'},
+ 'Alternate Names': {0: 'Diriamba,Diriambo',
+  1: 'Dolores',
+  2: 'Corinto',
+  3: 'El Realejo,Realejo'},
+ 'Country Code': {0: 'NI', 1: 'NI', 2: 'NI', 3: 'NI'},
+ 'Country name EN': {0: 'Nicaragua',
+  1: 'Nicaragua',
+  2: 'Nicaragua',
+  3: 'Nicaragua'},
+ 'Population': {0: 35008, 1: 7065, 2: 19183, 3: 6208},
+ 'Timezone': {0: 'America/Managua',
+  1: 'America/Managua',
+  2: 'America/Managua',
+  3: 'America/Managua'},
+ 'Modification date': {0: '2018-08-08',
+  1: '2018-08-08',
+  2: '2018-08-08',
+  3: '2018-08-08'},
+ 'LABEL EN': {0: 'Nicaragua', 1: 'Nicaragua', 2: 'Nicaragua', 3: 'Nicaragua'},
+ 'Coordinates': {0: '11.85812, -86.23922',
+  1: '11.85672, -86.21552',
+  2: '12.4825, -87.17304',
+  3: '12.54333, -87.16517'},
+ 'Longitude': {0: '-86.23922', 1: '-86.21552', 2: '-87.17304', 3: '-87.16517'},
+ 'Latitude': {0: '11.85812', 1: '11.85672', 2: '12.4825', 3: '12.54333'}}
+```
--- geonames/__init__.py
+++ geonames/__init__.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from scipy.spatial import KDTree
 from importlib.resources import files
 
@@ -22,71 +23,332 @@ tree = KDTree(df[["Latitude","Longitude"]].values)
 # [Longitude, Latitude]
 # can also input a list of Longitude and Latitude
 # [[Longitude1, Latitude1], [Longitude2, Latitude2]]
-def search(coordinates:[float,float]) -> pd.DataFrame:
+def search(coordinates:[list], k:int=1, output:dict | pd.DataFrame='df', ascending:bool=None) -> dict | pd.DataFrame:
     """
-    Function will search geonames data for closest city for the inputted Longitude and Latitude
+    This function will search GeoNames dataset for the closest city given the inputted Longitude and Latitude coordinates.
 
     PARAMETERS
     ----------
-    coordinates: (Required) Search query allows a list of Latitude and Longitude, or a single pair which must be passed as list. It will return name of the nearest city in the same order which the coordinates were passed.
+    - coordinates: Required - list or list(list) 
+        
+        Search query allows a list of Latitude and Longitude, or a single pair which must be passed as list. It will return name of the nearest city in the same order which the coordinates were passed.
+        
+        Example:
+
+        single location: coordinates = [11.86, -86.24]
+
+        multiple locations: coordinates = [[11.86, -86.24], [42, 49]]
     
+    - k: Optional - int > 1, Default: int = 1
+        
+        Number of of nearest neighbours to find for each set of coordinates. Must be a value greater than zero.
+    
+    - output: Optional - 'df' or 'dict', default: output = 'df'
+        
+        Which format to return to users. Either pandas DataFrame or dictionary.
+    
+    - ascending: Optional - True or False, default: None
+
+        If the format should be sorted by ascending, descending population, or no sorting at all. This can help in finding the nearest city with the largest population (might need to increase k).
+        
     RETURNS
     ----------
-    Dictionary{Dictionary}: Key:Value{Dictionary} - info
-        'Geoname ID':
-        'Name': - (City Name)
-        'ASCII Name': - (City Name in ASCII)
-        'Alternate Names': - (Alternate names for the city, or a list of alternate names)
-        'Country Code':
-        'Country name EN':
-        'Population':
-        'Timezone':
-        'Modification date': - (Date this data point was updated)
-        'LABEL EN':
-        'Coordinates':
-        'Longitude':
-        'Latitude':
+    - Dictionary{Dictionary}: Key:Value{Dictionary} - info
+        
+    - 'Geoname ID'
+    - 'Name' - (City Name)
+    - 'ASCII Name' - (City Name in ASCII)
+    - 'Alternate Names' - (Alternate names for the city, or a list of alternate names)
+    - 'Country Code'
+    - 'Country name EN'
+    - 'Population'
+    - 'Timezone'
+    - 'Modification date' - (Date this data point was updated)
+    - 'LABEL EN'
+    - 'Coordinates'
+    - 'Longitude'
+    - 'Latitude'
 
     EXAMPLES
     ----------
+
+    - Search for the nearest city's name from a single coordinate.
+
     >>> import pygeocity
-    >>> pygeocity.search([11.85812, -86.23922])
+    >>> geonames.search([11.85812, -86.23922])
 
     Outputs:
-        {
-            0: 3620170,
-            1: 'Diriamba',
-            2: 'Diriamba',
-            3: 'Diriamba,Diriambo',
-            4: 'NI',
-            5: 'Nicaragua',
-            6: 35008,
-            7: 'America/Managua',
-            8: '2018-08-08',
-            9: 'Nicaragua',
-            10: '11.85812, -86.23922',
-            11: ' -86.23922',
-            12: '11.85812'
-        }
 
-    >>> pygeocity.search([11.85812, -86.23922],[12.4825, -87.17304])
+    {'Geoname ID': {0: 3620170},
+
+    'Name': {0: 'Diriamba'},
+
+    'ASCII Name': {0: 'Diriamba'},
+
+    'Alternate Names': {0: 'Diriamba,Diriambo'},
+
+    'Country Code': {0: 'NI'},
+
+    'Country name EN': {0: 'Nicaragua'},
+
+    'Population': {0: 35008},
+
+    'Timezone': {0: 'America/Managua'},
+
+    'Modification date': {0: '2018-08-08'},
+
+    'LABEL EN': {0: 'Nicaragua'},
+
+    'Coordinates': {0: '11.85812, -86.23922'},
+
+    'Longitude': {0: '-86.23922'},
+
+    'Latitude': {0: '11.85812'}}
+
+    - Search for the nearest city's name from two coordinates.
+
+    >>> geonames.search([11.85812, -86.23922],[12.4825, -87.17304])
 
     Outputs:
-        {
-            'Geoname ID': {0: 3620170, 1: 3620269},
-            'Name': {0: 'Diriamba', 1: 'Corinto'},
-            'ASCII Name': {0: 'Diriamba', 1: 'Corinto'},
-            'Alternate Names': {0: 'Diriamba,Diriambo', 1: 'Corinto'},
-            'Country Code': {0: 'NI', 1: 'NI'},
-            'Country name EN': {0: 'Nicaragua', 1: 'Nicaragua'},
-            'Population': {0: 35008, 1: 19183},
-            'Timezone': {0: 'America/Managua', 1: 'America/Managua'},
-            'Modification date': {0: '2018-08-08', 1: '2018-08-08'},
-            'LABEL EN': {0: 'Nicaragua', 1: 'Nicaragua'},
-            'Coordinates': {0: '11.85812, -86.23922', 1: '12.4825, -87.17304'},
-            'Longitude': {0: ' -86.23922', 1: ' -87.17304'},
-            'Latitude': {0: '11.85812', 1: '12.4825'}
-        }
+    {'CoordIndex': {0: 1, 1: 0},
+
+    'Geoname ID': {0: 584715, 1: 3620170},
+
+    'Name': {0: 'Xudat', 1: 'Diriamba'},
+
+    'ASCII Name': {0: 'Xudat', 1: 'Diriamba'},
+
+    'Alternate Names': {0: 'Khudaf,Khudat,Xudat,Худат', 1: 'Diriamba,Diriambo'},
+
+    'Country Code': {0: 'AZ', 1: 'NI'},
+
+    'Country name EN': {0: 'Azerbaijan', 1: 'Nicaragua'},
+
+    'Population': {0: 13625, 1: 35008},
+
+    'Timezone': {0: 'Asia/Baku', 1: 'America/Managua'},
+
+    'Modification date': {0: '2014-06-26', 1: '2018-08-08'},
+
+    'LABEL EN': {0: 'Azerbaijan', 1: 'Nicaragua'},
+
+    'Coordinates': {0: '41.63052, 48.68161', 1: '11.85812, -86.23922'},
+
+    'Longitude': {0: '48.68161', 1: '-86.23922'},
+
+    'Latitude': {0: '41.63052', 1: '11.85812'}}
+
+    - Search for the two nearest cities name from two coordinates.
+
+    >>> geonames.search([[11.85812, -86.23922],[12.4825, -87.17304]], k=2)
+
+    Outputs:
+
+    {'CoordIndex': {0: 0, 1: 0, 2: 1, 3: 1},
+
+    'Geoname ID': {0: 3620163, 1: 3620170, 2: 498904, 3: 584715},
+
+    'Name': {0: 'Dolores', 1: 'Diriamba', 2: 'Samur', 3: 'Xudat'},
+
+    'ASCII Name': {0: 'Dolores', 1: 'Diriamba', 2: 'Samur', 3: 'Xudat'},
+
+    'Alternate Names': {0: 'Dolores',
+
+    1: 'Diriamba,Diriambo',
+
+    2: 'Samur,Самур',
+
+    3: 'Khudaf,Khudat,Xudat,Худат'},
+    'Country Code': {0: 'NI', 1: 'NI', 2: 'RU', 3: 'AZ'},
+
+    'Country name EN': {0: 'Nicaragua',
+
+    1: 'Nicaragua',
+
+    2: 'Russian Federation',
+
+    3: 'Azerbaijan'},
+
+    'Population': {0: 7065, 1: 35008, 2: 3730, 3: 13625},
+
+    'Timezone': {0: 'America/Managua',
+
+    1: 'America/Managua',
+
+    2: 'Europe/Moscow',
+
+    3: 'Asia/Baku'},
+
+    'Modification date': {0: '2018-08-08',
+
+    1: '2018-08-08',
+
+    2: '2012-01-17',
+
+    3: '2014-06-26'},
+
+    'LABEL EN': {0: 'Nicaragua',
+
+    1: 'Nicaragua',
+
+    2: 'Russian Federation',
+
+    ...
+
+    1: '11.85812, -86.23922',
+
+    2: '41.82527, 48.48597',
+
+    3: '41.63052, 48.68161'},
+
+    'Longitude': {0: '-86.21552', 1: '-86.23922', 2: '48.48597', 3: '48.68161'},
+
+    'Latitude': {0: '11.85672', 1: '11.85812', 2: '41.82527', 3: '41.63052'}}
+
+    - Search for the two nearest cities name from two coordinates and return the output with the cities population in descending order.
+
+    >>> geonames.search([[11.85812, -86.23922],[12.4825, -87.17304]], k=2, ascending=False)
+
+    {'CoordIndex': {0: 0, 1: 0, 2: 1, 3: 1},
+
+    'Geoname ID': {0: 3620170, 1: 3620163, 2: 584715, 3: 498904},
+
+    'Name': {0: 'Diriamba', 1: 'Dolores', 2: 'Xudat', 3: 'Samur'},
+
+    'ASCII Name': {0: 'Diriamba', 1: 'Dolores', 2: 'Xudat', 3: 'Samur'},
+
+    'Alternate Names': {0: 'Diriamba,Diriambo',
+
+    1: 'Dolores',
+
+    2: 'Khudaf,Khudat,Xudat,Худат',
+
+    3: 'Samur,Самур'},
+
+    'Country Code': {0: 'NI', 1: 'NI', 2: 'AZ', 3: 'RU'},
+
+    'Country name EN': {0: 'Nicaragua',
+
+    1: 'Nicaragua',
+
+    2: 'Azerbaijan',
+
+    3: 'Russian Federation'},
+
+    'Population': {0: 35008, 1: 7065, 2: 13625, 3: 3730},
+
+    'Timezone': {0: 'America/Managua',
+
+    1: 'America/Managua',
+
+    2: 'Asia/Baku',
+
+    3: 'Europe/Moscow'},
+
+    'Modification date': {0: '2018-08-08',
+
+    1: '2018-08-08',
+
+    2: '2014-06-26',
+    
+    3: '2012-01-17'},
+
+    'LABEL EN': {0: 'Nicaragua',
+
+    1: 'Nicaragua',
+
+    2: 'Azerbaijan',
+
+    ...
+
+    1: '11.85672, -86.21552',
+
+    2: '41.63052, 48.68161',
+
+    3: '41.82527, 48.48597'},
+
+    'Longitude': {0: '-86.23922', 1: '-86.21552', 2: '48.68161', 3: '48.48597'},
+
+    'Latitude': {0: '11.85812', 1: '11.85672', 2: '41.63052', 3: '41.82527'}}
     """
-    _, index = tree.query(coordinates, k=1)
-    return df.iloc[index].reset_index(drop=True).to_dict()
+    try:
+        flat_list = np.array(coordinates).flatten()
+    except ValueError as error:
+        print(error)
+    else:
+        match output:
+            case 'dict':
+                if len(flat_list) == 2 and k == 1:
+                    _, index = tree.query(coordinates, k=k)
+                    if ascending != None:
+                        return df.iloc[[index]].reset_index(drop=True).sort_values(by='Population', ascending=ascending).to_dict()
+                    else:
+                        return df.iloc[[index]].reset_index(drop=True).to_dict()
+                elif len(flat_list) == 2 and k > 1:
+                    _, index = tree.query(coordinates, k=k)
+                    return df.iloc[index].reset_index(drop=True).to_dict()
+                else:
+                    placeData = pd.DataFrame()
+                    _, nn = tree.query(coordinates, k=k)
+                    if k == 1:
+                        temp_Df = pd.DataFrame()
+                        for i, index in enumerate(nn):
+                            tempDf = df.iloc[[index]].copy(deep=False)
+                            tempDf.insert(loc=0, column='CoordIndex', value=i)
+                            if ascending != None:
+                                temp_Df = pd.concat([temp_Df, tempDf]).sort_values(by='Population', ascending=ascending)
+                            else:
+                                temp_Df = pd.concat([temp_Df, tempDf]).sort_values(by='Population')
+                        placeData = pd.concat([placeData, temp_Df])
+                    else:
+                        for i, indexes in enumerate(nn):
+                            temp_Df = pd.DataFrame()
+                            for index in indexes:
+                                tempDf = df.iloc[[index]].copy(deep=False)
+                                tempDf.insert(loc=0, column='CoordIndex', value=i)
+                                if ascending != None:
+                                    temp_Df = pd.concat([temp_Df, tempDf]).sort_values(by='Population', ascending=ascending)
+                                else:
+                                    temp_Df = pd.concat([temp_Df, tempDf]).sort_values(by='Population')
+                            placeData = pd.concat([placeData, temp_Df])
+                    placeData.reset_index(drop=True, inplace=True)
+                    return placeData.to_dict()
+            case 'df':
+                if len(flat_list) == 2 and k == 1:
+                    _, index = tree.query(coordinates, k=k)
+                    if ascending != None:
+                        return df.iloc[[index]].reset_index(drop=True).sort_values(by='Population', ascending=ascending)
+                    else:
+                        return df.iloc[[index]].reset_index(drop=True)
+                elif len(flat_list) == 2 and k > 1:
+                    _, index = tree.query(coordinates, k=k)
+                    return df.iloc[index].reset_index(drop=True)
+                else:
+                    placeData = pd.DataFrame()
+                    _, nn = tree.query(coordinates, k=k)
+                    if k == 1:
+                        temp_Df = pd.DataFrame()
+                        for i, index in enumerate(nn):
+                            tempDf = df.iloc[[index]].copy(deep=False)
+                            tempDf.insert(loc=0, column='CoordIndex', value=i)
+                            if ascending != None:
+                                temp_Df = pd.concat([temp_Df, tempDf]).sort_values(by='Population', ascending=ascending)
+                            else:
+                                temp_Df = pd.concat([temp_Df, tempDf]).sort_values(by='Population')
+                        placeData = pd.concat([placeData, temp_Df])
+                    else:
+                        for i, indexes in enumerate(nn):
+                            temp_Df = pd.DataFrame()
+                            for index in indexes:
+                                tempDf = df.iloc[[index]].copy(deep=False)
+                                tempDf.insert(loc=0, column='CoordIndex', value=i)
+                                if ascending != None:
+                                    temp_Df = pd.concat([temp_Df, tempDf]).sort_values(by='Population', ascending=ascending)
+                                else:
+                                    temp_Df = pd.concat([temp_Df, tempDf]).sort_values(by='Population')
+                            placeData = pd.concat([placeData, temp_Df])
+                    placeData.reset_index(drop=True, inplace=True)
+                    return placeData
+            # If an exact match is not confirmed, this last case will be used if provided
+            case _:
+                raise Exception("output parameter has to be of type string and either 'df' for pandas DataFrame or 'dict' for a dictionary.")
